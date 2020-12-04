f0aef2d0cdebe87aa71f61aa9e1b23936f883acf
kouym7979 <52284829+kouym7979@users.noreply.github.com>
2020-12-04 15:32:24 +0900

    [add]search_title function
    제목을 기준으로 포함된 게시글을 새로 출력합니다.

--- app/src/main/java/com/example/sm_capstone/DynamicBoard.java
+++ app/src/main/java/com/example/sm_capstone/DynamicBoard.java
@@ -8,8 +8,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.example.sm_capstone.Board_Post.Post;
@@ -33,11 +37,12 @@ public class DynamicBoard extends AppCompatActivity implements View.OnClickListe
     private Context context;
     private RecyclerView Board;//동적게시판
     private BoardAdapter mAdapter;
-    private List<Post> mDatas;
+    private List<Post> mDatas,sub;
     private String board_part;
     private RecyclerView.LayoutManager mlayoutManager;
     private FirebaseFirestore mStore=FirebaseFirestore.getInstance();
     private Button write_btn;
+    private EditText Search_edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,7 +54,25 @@ public class DynamicBoard extends AppCompatActivity implements View.OnClickListe
         Board.setLayoutManager(mlayoutManager);
 
         write_btn.setOnClickListener(this);
+        Search_edit=findViewById(R.id.search_edit);
 
+        Search_edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = Search_edit.getText().toString();
+                search(text);
+            }
+        });
     }
     @Override
     protected void onStart() {
@@ -57,6 +80,7 @@ public class DynamicBoard extends AppCompatActivity implements View.OnClickListe
         board_part=intent.getStringExtra("board_part");//정적 또는 동적게시판 데이터를 불러옴
         super.onStart();
         mDatas = new ArrayList<>();//
+        sub=new ArrayList<>();
         mStore.collection("Post")//리사이클러뷰에 띄울 파이어베이스 테이블 경로
                 .whereEqualTo("board_part",board_part)//후에 가게정보에 따른 비교를 추가해야함
                 .orderBy(EmployID.timestamp, Query.Direction.DESCENDING)//시간정렬순으로
@@ -77,6 +101,7 @@ public class DynamicBoard extends AppCompatActivity implements View.OnClickListe
                                         String board_part=String.valueOf(shot.get(EmployID.board_part));
                                         Post data = new Post(documentId, title, contents,post_id,writer_name,post_photo,board_part);
                                         mDatas.add(data);//여기까지가 게시글에 해당하는 데이터 적용
+                                        sub.add(data);
                                     }
                                     mAdapter = new BoardAdapter(DynamicBoard.this,mDatas);//mDatas라는 생성자를 넣어줌
                                     Board.setAdapter(mAdapter);
@@ -90,5 +115,33 @@ public class DynamicBoard extends AppCompatActivity implements View.OnClickListe
         intent.putExtra("board_part",board_part);
         startActivity(intent);
         finish();
+    }
+    public void search(final String s_text){
+        mDatas.clear();//문자 입력시새로 리스트 목록 활성화를 위해서
+
+        if(s_text.length()==0)
+            mDatas.addAll(sub);//다시 기존의 데이터를 넣어줌
+        else{
+                for (int i=0;i<sub.size();i++) {
+                    String s_title=sub.get(i).getTitle();
+                    Log.d("확인","제목:"+s_title);
+                    Log.d("확인","s_text: "+s_text);
+                    if (s_title.contains(s_text)) {
+                        String documentId = String.valueOf(sub.get(i).getDocumentId());
+                        String title = String.valueOf(sub.get(i).getTitle());
+                        String contents = String.valueOf(sub.get(i).getContents());
+                        String writer_name = String.valueOf(sub.get(i).getWriter_name());
+                        String post_id=String.valueOf(sub.get(i).getPost_id());
+                        String post_photo=String.valueOf(sub.get(i).getPost_photo());
+                        String board_part=String.valueOf(sub.get(i).getBoard_part());
+                        Post data = new Post(documentId, title, contents,post_id,writer_name,post_photo,board_part);
+                        mDatas.add(data);
+                        Log.d("확인","포함되어있습니다"+s_title);
+                    }
+                    else Log.d("확인","확인이 안됩니다");
+                }
+            }
+
+        mAdapter.notifyDataSetChanged();
     }
 }
