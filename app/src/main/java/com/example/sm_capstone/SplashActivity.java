30324612a0eb58e4dbaaad0049a990da620dedd8
kouym7979 <52284829+kouym7979@users.noreply.github.com>
2020-12-09 20:46:49 +0900

    [update]Hashkey
    해시키 업데이트

--- app/src/main/java/com/example/sm_capstone/SplashActivity.java
+++ app/src/main/java/com/example/sm_capstone/SplashActivity.java
@@ -19,6 +19,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.kakao.util.helper.Utility;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -26,28 +27,51 @@ import java.security.NoSuchAlgorithmException;
 import static com.kakao.util.helper.Utility.getPackageInfo;
 
 public class SplashActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth=FirebaseAuth.getInstance();
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
     private String store_num;
 
-    //GetKeyHash getKeyHash =new GetKeyHash(this);
+    //GetKeyHash getKeyHash =new GetKeyHash();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 //        getSupportActionBar().hide();
 
-        Handler hd=new Handler();
-        hd.postDelayed(new splashhandler(),1000);
+        Handler hd = new Handler();
+        hd.postDelayed(new splashhandler(), 1000);
+        //getKeyHash();
+        getHashKey();
     }
 
     private class splashhandler implements Runnable {
-        public void run(){
-            startActivity(new Intent(getApplication(),LoginActivity.class));
+        public void run() {
+            startActivity(new Intent(getApplication(), LoginActivity.class));
             SplashActivity.this.finish();
         }
     }
+    
+    private void getHashKey(){
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
 
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
+    }
 
 
 }
